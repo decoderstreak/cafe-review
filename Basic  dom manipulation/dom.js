fbd1bccf7d8027c478cd2f362e088621195af31f
Decoder streak <uman.tab12@gmail.com>
2021-05-30 11:36:28 +0530

    dom

---
+++ Basic  dom manipulation/dom.js
@@ -0,0 +1,13 @@
+console.log("hii tabnine");
+const cleo =document.getElementById("cleo");
+cleo.style.color="red"
+// const cls =document.createElement("div");
+// const hhd = document.getElementsByClassName();
+// hhd.appendChild("hii")
+document.getElementById("dem").addEventListener("mousemove", displayDate);
+
+ function displayDate() {
+  alert(Date())
+ }
+
+ //Cool I got the dom manipulation techniques
